c3471ec96bd9af601d1864d8ce38c4958601cc44
Rovaletti <santiago.rovaletti@senado.gob.ar>
2024-08-27 18:53:56 -0300

    add reset-password funcionality

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
